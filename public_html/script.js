ac77fd476c6dc6b3320f19e94975e80a0fbfc224
SidhuSupreet <ssidhu@stevens.edu>
2022-03-15 12:48:05 -0400

    added self collision, restart, and highscore

--- public_html/script.js
+++ public_html/script.js
@@ -45,6 +45,10 @@ function setTheme(mode){
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext('2d');
 
+if(!localStorage.getItem('highScore')){
+	localStorage.setItem('highScore', 0);
+}
+
 class snakePart{
 	constructor(x,y){
 		this.x = x;
@@ -64,6 +68,9 @@ let velX = 0;
 let velY = 0;
 const snakeParts = [];
 let tailLen = 2;
+let prevVelX = 0;
+let prevVelY = 0;
+
 
 //food attributes
 let foodX = 5;
@@ -72,6 +79,21 @@ let foodY = 5;
 let score = 0;
 
 function drawGame(){
+	if(prevVelX === 1 && velX === -1){
+		velX = prevVelX;
+	}
+	if(prevVelX === -1 && velX === 1){
+		velX = prevVelX;
+	}
+	if(prevVelY === 1 && velY === -1){
+		velY = prevVelY;
+	}
+	if(prevVelY === -1 && velY === 1){
+		velY = prevVelY;
+	}
+	prevVelX = velX;
+	prevVelY = velY;
+
 	changeSnakePosition();
 	let result = isGameOver();
 	if(result){
@@ -88,6 +110,10 @@ function drawGame(){
 function isGameOver() {
 	let gameOver = false;
 
+	if(velX === 0 && velY === 0){
+		return false;
+	}
+
 	if(headX < 0 || headX === tileCount || headY < 0 || headY === tileCount){
 		gameOver = true;
 	}
@@ -101,9 +127,19 @@ function isGameOver() {
 	}
 
 	if (gameOver) {
+		if(score > localStorage.getItem('highScore')){
+			localStorage.setItem('highScore', score);
+		}
 		ctx.fillStyle = 'white';
 		ctx.font = "50px Verdana"
 		ctx.fillText("Game Over!", canvas.width /6.5, canvas.height/2);
+		ctx.font = "17px Verdana"
+		ctx.fillText("Press Enter to Restart", canvas.width/6.5 + 50, (canvas.height/2)+30);
+		document.body.addEventListener('keydown', function restart(event){
+			if(event.keyCode == 13) {
+				location.reload();
+			}
+		});
 	}
 	
 	return gameOver;
@@ -124,8 +160,14 @@ function checkFoodCollision() {
 	if(headX === foodX && headY === foodY){
 		foodX = Math.floor(Math.random() * tileCount);
 		foodY = Math.floor(Math.random() * tileCount);
+		if(foodY <= 2){
+			foodY += 2;
+		}
 		tailLen++;
 		score++;
+		if(score > localStorage.getItem('highScore')){
+			localStorage.setItem('highScore', score);
+		}
 	}
 }
 
@@ -147,13 +189,17 @@ function drawSnake() {
 	}
 
 	ctx.fillStyle = 'orange'
+	ctx.strokeStyle = 'orange'
 	ctx.fillRect(headX * tileCount, headY * tileCount, tileSize, tileSize);
 }
 
 function drawScore() {
 	ctx.fillStyle = 'white';
 	ctx.font = "10px Verdana"
-	ctx.fillText("Score: " + score, canvas.width-50, 10);
+	ctx.fillText("Score: " + score, 20, 20);
+	ctx.fillText("High Score: " + localStorage.getItem('highScore'), canvas.width-100, 20);
+
+
 }
 
 
@@ -167,30 +213,30 @@ window.addEventListener("keydown", function(e) {
 
 function keyDown(event){
 	//up
-	if(event.keyCode == 38){
-		if (velY == 1)
-			return;
+	if(event.keyCode == 38 || event.keyCode == 87){
+		// if (velY == 1)
+		// 	return;
 		velY = -1;
 		velX = 0;
 	}
 	//down
-	if(event.keyCode == 40){
-		if (velY == -1)
-			return;
+	if(event.keyCode == 40 || event.keyCode == 83){
+		// if (velY == -1)
+		// 	return;
 		velY = 1;
 		velX = 0;
 	}
-	//down
-	if(event.keyCode == 37){
-		if (velX == 1)
-			return;
+	//left
+	if(event.keyCode == 37 || event.keyCode == 65){
+		// if (velX == 1)
+		// 	return;
 		velY = 0;
 		velX = -1;
 	}
-	//down
-	if(event.keyCode == 39){
-		if (velX == -1)
-			return;
+	//right
+	if(event.keyCode == 39 || event.keyCode == 68){
+		// if (velX == -1)
+		// 	return;
 		velY = 0;
 		velX = 1;
 	}
